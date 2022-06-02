6546b40c29f6e13de89f8d82475249dca80b24e6
Tenai <kipronotena@gmail.com>
2022-06-02 22:09:07 +0300

    beta version

--- Client/client_changepassword_api.php
+++ Client/client_changepassword_api.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Content-Type: application/json, text/plain, x-www-form-urlencoded; charset=UTF-8*/*');
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-type, origin,  accept, x-requested-with");
-require 'config.php';
+require './../config.php';
 
 $json = file_get_contents('php://input');
 $data = json_decode($json);
